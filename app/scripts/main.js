9883854cd2915e7c0311750ed6dd7b0cb64af305
JaydenLin <j5905653@gmail.com>
2013-12-25 00:11:11 +0800

    handlebars

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,13 +16,17 @@ require.config({
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        handlebars: {
+            exports: 'Handlebars'
         }
     },
     paths: {
         jquery: '../bower_components/jquery/jquery',
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
-        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap'
+        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
+        handlebars: '../bower_components/handlebars/handlebars'
     }
 });
 
